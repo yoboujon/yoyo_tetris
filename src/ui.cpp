b1c3df7dbeb97352c92408e5f2e115e7b3998afa
Yohan Boujon <yo.boujon@gmail.com>
2023-08-20 22:37:04 +0200

    Score displayed at the end of the game.

--- src/ui.cpp
+++ src/ui.cpp
@@ -12,7 +12,7 @@ tetrisUI::tetrisUI(tetrisEvent* event, float* elapsedPtr)
     , _multiplicator(0)
     , _exit(false)
     , _newGame(false)
-    //, _kotoPiege(0.0f)
+//, _kotoPiege(0.0f)
 {
     // Init Textures
     _Texture_button = LoadTexture("res/button.png"); // Load button texture
@@ -103,9 +103,9 @@ void tetrisUI::Display(renderLayer layer)
     // Events to update variables
     uint64_t tempScore;
     uint8_t tempMultiplicator;
-    if(_eventPtr->OnEvent(eventType::SEND_SCORE, eventUser::UI, tempScore))
+    if (_eventPtr->OnEvent(eventType::SEND_SCORE, eventUser::UI, tempScore))
         _score = tempScore;
-    if(_eventPtr->OnEvent(eventType::SEND_MULTIPLICATOR, eventUser::UI, tempMultiplicator))
+    if (_eventPtr->OnEvent(eventType::SEND_MULTIPLICATOR, eventUser::UI, tempMultiplicator))
         _multiplicator = tempMultiplicator;
 }
 
@@ -156,17 +156,17 @@ void tetrisUI::Game()
     // This way Even in other resolutions the game should be designed the same way.
 
     Rectangle UIrectangle = { (_Rect_tetrisStage.x) - TILE_RATIO, (_Rect_tetrisStage.y) - TILE_RATIO, (_Rect_tetrisStage.width) + TILE_RATIO * 2, (_Rect_tetrisStage.height) + TILE_RATIO * 2 };
-    DrawText("Score:", static_cast<int>(TILE_RATIO*9), static_cast<int>(TILE_RATIO*9), 30, DARKGRAY);
-    DrawText(std::to_string(_score).c_str(), static_cast<int>(TILE_RATIO*9), static_cast<int>(TILE_RATIO*17), 30, DARKGRAY);
-    DrawText("Next:", static_cast<int>(TILE_RATIO*97), static_cast<int>(TILE_RATIO*9), 30, DARKGRAY);
-    if(_multiplicator > 1)
-        DrawText(("Multiplicator: x"+std::to_string(_multiplicator)).c_str(), static_cast<int>(TILE_RATIO*9), static_cast<int>(TILE_RATIO*25), 20, DARKGRAY);
+    DrawText("Score:", static_cast<int>(TILE_RATIO * 9), static_cast<int>(TILE_RATIO * 9), 30, DARKGRAY);
+    DrawText(std::to_string(_score).c_str(), static_cast<int>(TILE_RATIO * 9), static_cast<int>(TILE_RATIO * 17), 30, DARKGRAY);
+    DrawText("Next:", static_cast<int>(TILE_RATIO * 97), static_cast<int>(TILE_RATIO * 9), 30, DARKGRAY);
+    if (_multiplicator > 1)
+        DrawText(("Multiplicator: x" + std::to_string(_multiplicator)).c_str(), static_cast<int>(TILE_RATIO * 9), static_cast<int>(TILE_RATIO * 25), 20, DARKGRAY);
     // x=3 and y=4 for the offset because of the actual texture.
     // 3*7+4 = 24 -> 3 Tiles + 4 border -> What we want to draw for the next
-    DrawTextureRatio(_Texture_tileset_b, { 3.0f, 4.0f }, { 488, 118, 25*TILE_RATIO, 25*TILE_RATIO }, TILE_RATIO, { 0.0f, 0.0f }, WHITE);
+    DrawTextureRatio(_Texture_tileset_b, { 3.0f, 4.0f }, { 488, 118, 25 * TILE_RATIO, 25 * TILE_RATIO }, TILE_RATIO, { 0.0f, 0.0f }, WHITE);
     DrawTextureRatio(_Texture_tileset_b, { 3.0f, 4.0f }, UIrectangle, TILE_RATIO, { 0.0f, 0.0f }, WHITE);
     // ! 3.8f is for now a dark magic number, it seems to be linked with the 4.0f offset of the texture, but why 0.2 ?
-    DrawTextureRatio(_Texture_tileset_b_borderless, { 3.0f, 3.8f }, _Rect_tetrisStage, TETRIS_STAGE.width/(CASE_NUM*8), { 0.0f, 0.0f }, WHITE);
+    DrawTextureRatio(_Texture_tileset_b_borderless, { 3.0f, 3.8f }, _Rect_tetrisStage, TETRIS_STAGE.width / (CASE_NUM * 8), { 0.0f, 0.0f }, WHITE);
 }
 
 void tetrisUI::GameOver()
@@ -180,7 +180,9 @@ void tetrisUI::GameOver()
 
     // Drawing UI
     DrawRectangleRec({ 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT }, PAUSE_COLOR);
-    DrawText("Game Over", (SCREEN_WIDTH - gameOvertext) / 2, (SCREEN_HEIGHT / 2) - 100, 30, RAYWHITE);
+    DrawText("Game Over", (SCREEN_WIDTH - gameOvertext) / 2, (SCREEN_HEIGHT / 2) - 150, 30, RAYWHITE);
+    const std::string scoreText = "Final score: " + std::to_string(_score);
+    DrawText(scoreText.c_str(), (SCREEN_WIDTH - MeasureText(scoreText.c_str(), 20)) / 2, (SCREEN_HEIGHT / 2) - 100, 20, GOLD);
 
     // Button Actions
     _Btn_titleScreen.Update();
